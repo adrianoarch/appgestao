138b21930efad8480abb76bc6a4bb867de292313
Adriano de Oliveira <adriano.98@gmail.com>
2022-10-05 14:06:58 -0300

    Adicionando middleware autenticacao e testando nas rotas

--- routes/web.php
+++ routes/web.php
@@ -23,9 +23,15 @@ Route::post('/contato', [ContatoController::class, 'salvar'])->name('site.contat
 Route::get('/login', function(){return 'Login';})->name('site.login');
 
 Route::prefix('/app')->group(function() {
-    Route::get('/clientes', function(){return 'Clientes';})->name('app.clientes');
-    Route::get('/fornecedores', [FornecedorController::class, 'index'])->name('app.fornecedores');
-    Route::get('/produtos', function(){return 'produtos';})->name('app.produtos');
+    Route::get('/clientes', function(){return 'Clientes';})
+    ->name('app.clientes')
+    ->middleware('autenticacao');
+    Route::get('/fornecedores', [FornecedorController::class, 'index'])
+    ->name('app.fornecedores')
+    ->middleware('autenticacao');
+    Route::get('/produtos', function(){return 'produtos';})
+    ->name('app.produtos')
+    ->middleware('autenticacao');
 });
 
 Route::get('/teste/{p1}/{p2}', [TesteController::class, 'teste'])->name('site.teste');
