e2a0004b6021b6701338bc7f8b018dda57cbb69f
Adriano de Oliveira <adriano.98@gmail.com>
2022-10-07 13:51:07 -0300

    COnfigurando middleware para verificar autenticacao

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+
+class LoginController extends Controller
+{
+    public function index(Request $request){
+        $erro = '';
+        
+        if($request->get('erro') == 1){
+            echo 'Usuário não existe';
+        }
+        if($request->get('erro') == 2){
+            $erro = 'Você precisa estar logado no sistema para acessar esta página';
+        }
+        return view('site.login', ['titulo' => 'Login', 'erro' => $erro]);
+    }
+
+    public function autenticar(Request $request){
+        $regras = [
+            'usuario' => 'email',
+            'senha' => 'required'
+        ];
+        
+        $feedback = [
+            'usuario.email' => 'O campo usuário precisa ser um e-mail válido',
+            'senha.required' => 'O campo senha é obrigatório'
+        ];
+        
+        $request->validate($regras, $feedback);
+        
+        $usuario = $request->input('usuario');
+        $senha = $request->input('senha');
+
+        $user = new User();
+        $verificaUsuario = $user
+            ->where('email', $usuario)
+            ->where('password', $senha)
+            ->get()
+            ->first();
+
+        if(isset($verificaUsuario->name)){
+            session_start();
+            $_SESSION['nome'] = $verificaUsuario->name;
+            $_SESSION['email'] = $verificaUsuario->email;
+            // dd($_SESSION);
+            return redirect()->route('app.clientes');
+        }
+        else{
+            return redirect()->route('site.login')->with('erro', 'Usuário ou senha inválidos');
+        }
+
+        // return redirect()->route('site.login');
+
+    }
+}
